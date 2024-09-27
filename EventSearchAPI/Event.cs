89302f30332a3fb99bc4aced609ea69f9257f38c
Your Name <you@example.com>
2024-09-27 09:32:47 +0200

    Updated Events

--- EventSearchAPI/Event.cs
+++ EventSearchAPI/Event.cs
@@ -1,11 +1,18 @@
 ﻿namespace EventSearchAPI
 {
     public class Event
-    {
+    { 
+        // Should automatically generate a new guid
+        public Event() 
+        {
+            Id = Guid.NewGuid().ToString();
+        }
         public string Id { get; set; }
-        public string Name { get; set; }
+        public string eventName { get; set; }
+        public string Oraganiser { get; set; }
         public string Location { get; set; }
+        public string Category { get; set; }
         public DateTime Date { get; set; }
-        public string Description { get; set; } 
+        public string Details { get; set; } 
     }
 }
